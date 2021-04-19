212565779a968de7586b851388ed549ffd05d215
Nova+ <cszonalt@gmail.com>
2021-04-20 00:46:23 +0300

    schedule fix attempt; discord webhook log added

--- controllers/routine.js
+++ controllers/routine.js
@@ -8,15 +8,36 @@ console.log("Onload Skillboost Fetching...")
 fetch_skillboost(true);
 
 //schedule
-const rule = new schedule.RecurrenceRule();
-rule.tz = 'Etc/UTC';
-rule.hour = 0;
-rule.minute = 1;
-// rule.second = 2;
+(() => {
+    const rule = new schedule.RecurrenceRule();
+    rule.tz = 'Etc/UTC';
+    rule.hour = 0;
+    rule.minute = 1;
+    // rule.second = 2;
 
-const job = schedule.scheduleJob(rule, () => {
-    fetch_skillboost(true);
-    post_discord_log(`Skillboost schedule: next scan @ ${new Date()} -> ${JSON.stringify(skillboost_cache.data)}`);
-})
+    const job = schedule.scheduleJob(rule, () => {
+        fetch_skillboost(true);
+        post_discord_log(`[MAIN] Skillboost schedule: next scan @ ${new Date()} -> ${JSON.stringify(skillboost_cache.data)}`);
+    })
 
-post_discord_log(`[${String(process.env.NODE_ENV).toUpperCase()}]: Skillboost schedule: started! first scan will be at [${new Date(job?.nextInvocation()?._date?.ts)}]`);
+    post_discord_log(`[${String(process.env.NODE_ENV).toUpperCase()}]: [MAIN] Skillboost schedule: started! first scan will be at [${new Date(job?.nextInvocation()?._date?.ts)}]`);
+})();
+
+
+//backup schedule just in case the old data wasn't updated
+(() => {
+
+    const rule = new schedule.RecurrenceRule();
+    rule.tz = 'Etc/UTC';
+    rule.hour = 1;
+    rule.minute = 0;
+    // rule.second = 2;
+
+    const job = schedule.scheduleJob(rule, () => {
+        fetch_skillboost(true);
+        post_discord_log(`[BACKUP] Skillboost schedule: next scan @ ${new Date()} -> ${JSON.stringify(skillboost_cache.data)}`);
+    })
+
+    post_discord_log(`[${String(process.env.NODE_ENV).toUpperCase()}]: [BACKUP] Skillboost schedule: started! first scan will be at [${new Date(job?.nextInvocation()?._date?.ts)}]`);
+
+})();
